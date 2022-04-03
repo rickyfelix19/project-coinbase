be2ae992318b840a3fcb0cdc4b0d021738df0944
Ricky Felix <rickyfelix19@outlook.com>
2022-04-03 14:50:00 +1000

    create json file for NavBar

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,10 +1,55 @@
-import React from 'react'
-import { slide as Menu } from 'react-burger-menu';
+import React from 'react';
+import {
+  ProSidebar,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+  Menu,
+  SubMenu,
+  MenuItem
+} from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+
+// import SidebarItems from './SidebarItems.json'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div>
+      <ProSidebar>
+        <SidebarHeader>Coin Gecko</SidebarHeader>
+
+        <SidebarContent>
+          <Menu>
+            <SubMenu title="Coins">
+              <MenuItem>Market Cap Rank</MenuItem>
+              <MenuItem>Recently Added</MenuItem>
+              <MenuItem>Categories</MenuItem>
+              <MenuItem>Discover</MenuItem>
+              <MenuItem>Large Movers</MenuItem>
+              <MenuItem>High Volumes</MenuItem>
+              <SubMenu title="Derivatives">
+                <MenuItem>Large Movers</MenuItem>
+                <MenuItem>High Volumes</MenuItem>
+              </SubMenu>
+            </SubMenu>
+
+            <SubMenu title="Exchanges">
+              <MenuItem>Spot</MenuItem>
+              <MenuItem>DEX</MenuItem>
+              <MenuItem>Derivatives</MenuItem>
+            </SubMenu>
+
+            <hr />
+            <MenuItem>Portfolio</MenuItem>
+            <MenuItem>Login</MenuItem>
+            <MenuItem>Sign Up</MenuItem>
+          </Menu>
+        </SidebarContent>
+
+        <SidebarFooter>Ricky Felix</SidebarFooter>
+      </ProSidebar>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Sidebar from './components/sidebar/Sidebar';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Sidebar />
   </React.StrictMode>,
   document.getElementById('root')
 );
