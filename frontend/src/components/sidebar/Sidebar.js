c91282de24a13f5cf53c9001d03932076cc58cb1
Ricky Felix <rickyfelix19@outlook.com>
2022-04-03 15:39:43 +1000

    restructure sidebar json

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -10,15 +10,29 @@ import {
 } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 
-// import SidebarItems from './SidebarItems.json'
+import SidebarItems from './SidebarItems.json';
 
 function Sidebar() {
+  //   console.log(SidebarItems); json runs in DOM
+  //   console.log(SidebarItems.Sidebar);
   return (
     <div>
       <ProSidebar>
         <SidebarHeader>Coin Gecko</SidebarHeader>
 
         <SidebarContent>
+          {SidebarItems.Sidebar[0].map((data, index) => {
+            return (
+              <div key={index}>
+                <SubMenu title="title">
+                  <MenuItem>{data.items}</MenuItem>
+                </SubMenu>
+              </div>
+            );
+          })}
+        </SidebarContent>
+
+        {/* <SidebarContent>
           <Menu>
             <SubMenu title="Coins">
               <MenuItem>Market Cap Rank</MenuItem>
@@ -44,7 +58,7 @@ function Sidebar() {
             <MenuItem>Login</MenuItem>
             <MenuItem>Sign Up</MenuItem>
           </Menu>
-        </SidebarContent>
+        </SidebarContent> */}
 
         <SidebarFooter>Ricky Felix</SidebarFooter>
       </ProSidebar>
